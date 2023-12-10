3c6e163a30dc02b5178e03c341e48d30daa6cc3f
Alex Isaacs <alexisaacs18@gmail.com>
2023-12-09 23:53:51 -0500

    finished labs

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,35 @@
 #!/usr/bin/env python3
 
-def happy_new_year():
-    # code goes here!
-    pass
+def happy_new_year(num=10):
+    while num > 0:
+        print(num)
+        num -= 1
+    return print("Happy New Year!")
+
+
+happy_new_year()
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    new_int_list = [x ** 2 for x in int_list]
+    return new_int_list
+
+
+print(square_integers([-1, -2, -3, -4, -5]))
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    i = 1
+    while i < 101:
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+        i += 1
+
+
+fizzbuzz()
